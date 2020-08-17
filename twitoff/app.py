89cc2e2f98adbbd3c5427fb5b3d5cb72180450ac
dondreojordan <dondre-jordan@lambdastudents.com>
2020-08-17 10:27:35 -0700

    Twitoff Update, Errors with __init__.py

---
+++ twitoff/app.py
@@ -0,0 +1,15 @@
+"""Main app/routing file for Twitoff."""
+from flask import Flask, render_template
+
+
+def create_app():
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    
+    #...TODO make the app
+    @app.route('/')
+    def root():
+        return render_template('base.html', title='Home')
+        return 'Hello, Twitoff World!'
+    
+    return app
---
+++ twitoff/hello_example.py
@@ -0,0 +1,10 @@
+"""
+Minimal Example for Flask web aplication
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
